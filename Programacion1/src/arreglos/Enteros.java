9bd1360804829d6a14333e21ee44efd2cf5df838
Guido Baldini <guidobaldini.gb@gmail.com>
2025-08-20 15:01:23 -0300

    arreglos dentro de un proyecto

---
+++ Programacion1/src/arreglos/Enteros.java
@@ -0,0 +1,12 @@
+package arreglos;
+
+public class Enteros {
+
+	public static void main(String[] args) {
+		int[] ana = {10,45,-95,74,15,10,10,15,10,45};
+		double[] pepe = {10.5,45.9,789.25,741.56,45.2,479.10};
+		double[] d = Arreglos.pedirArrayD(5);
+		
+		Arreglos.imprimirArray(d);
+	}
+}
